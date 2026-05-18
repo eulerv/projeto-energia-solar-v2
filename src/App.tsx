b8a5edb26a711979aad44256c50a8653205acd8a
eulerv <euler.vicente@hotmail.com>
2026-05-18 13:08:25 -0300

    Update componente do analytics da vercel

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import About from "./pages/About.tsx";
 import Index from "./pages/Index.tsx";
@@ -25,6 +26,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
